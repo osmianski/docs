f453be07508cc5316d3dd899c651326dbe5c9088
Vladislav Osmianskij <vlaosm@gmail.com>
2022-10-14 15:48:06 +0300

    fix: “Something went wrong” error

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -12,7 +12,8 @@ class LandingController extends Controller
                 http_build_query([
                     'client_id' => env('NOTION_CLIENT_ID', fn() =>
                         throw new Required("Register a public Notion integration, and put its client ID into NOTION_CLIENT_ID environment variable.")),
-                    'redirect_uri' => route('notion.auth-callback'),
+                    'redirect_uri' => env('NOTION_AUTH_CALLBACK_URL',
+                        route('notion.auth-callback')),
                     'owner' => 'user',
                     'response_type' => 'code',
                 ]),
