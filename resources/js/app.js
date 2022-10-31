68ca35b922601ea3a74950b0feea879d8b4ec74b
Vladislav Osmianskij <vlaosm@gmail.com>
2022-10-31 02:43:10 +0200

    fix: style

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
     title: (title) => `${title} - Docs`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`,
+        import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
