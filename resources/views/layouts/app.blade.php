5e038b5101d789ec1b5da34fd0577066a5bd7b05
Vlad Osmianski <vlaosm@gmail.com>
2023-04-17 22:52:40 +0300

    install Livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,17 @@
 <!doctype html>
-<html>
+<html class="h-full">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
-<body>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<body class="h-full">
+    <div class="min-h-full">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
 </body>
 </html>
---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+     <h1 class="text-5xl">Because she competes with no one, no one can compete with her.</h1>
+</div>
