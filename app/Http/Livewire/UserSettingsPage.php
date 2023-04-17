f480c7fa91a9b53b4d1e08cfb38e907401456efc
Vlad Osmianski <vlaosm@gmail.com>
2023-04-18 00:08:23 +0300

    WIP user settings page

--- app/Http/Livewire/UserSettingsPage.php
+++ app/Http/Livewire/UserSettingsPage.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class UserSettingsPage extends Component
 {
-    public function render()
+    public array $repository_mappings = [];
+
+    public function render() : View
     {
         return view('livewire.user-settings-page');
     }
