2801f5f5537da2fe1a3d445a6bc40b9822d0d19c
Vlad Osmianski <vlaosm@gmail.com>
2023-04-23 16:50:06 +0300

    Fix

--- resources/js/Pages/SignIn.vue
+++ resources/js/Pages/SignIn.vue
@@ -52,7 +52,7 @@ const form = useForm({
                     </div>
                     <div class="relative mt-2 rounded-md shadow-sm">
                         <input v-model="form.password" id="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        <div v-if="form.errors.email" class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <div v-if="form.errors.password" class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                             <ExclamationCircleIcon class="h-5 w-5 text-red-500" aria-hidden="true" />
                         </div>
                     </div>
