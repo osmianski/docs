e8163be1b63fa91fe0dda361023051b03218e4b9
Vlad Osmianski <vlaosm@gmail.com>
2023-04-23 08:43:26 +0300

    Code style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    public function show(){
+    public function show()
+    {
         return inertia('Home');
     }
 }
